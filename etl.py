09996c41446267a58ce2c7767e048fcefd196893
Caitlin Loftus <caitlinmloftus@gmail.com>
2021-02-22 17:34:08 -0600

    reworked download strategy to just download new data, and cleaned up code a bit

--- etl.py
+++ etl.py
@@ -10,37 +10,239 @@ import pandas as pd
 from googleapiclient.discovery import build
 from google.oauth2 import service_account
 
+PUF_USECOLS = [
+'AHHLD_NUMPER',
+ 'WHYCHNGD1',
+ 'SPNDSRC7',
+ 'HLTHINS5',
+ 'WHEREFREE7',
+ 'FORCLOSE',
+ 'FEWRTRIPS',
+ 'SSAPGM2',
+ 'MH_SVCS',
+ 'WHEREFREE4',
+ 'HLTHINS6',
+ 'TSPNDPRPD',
+ 'SSAPGM1',
+ 'EXPCTLOSS',
+ 'INTEREST',
+ 'SSAEXPCT2',
+ 'PSWHYCHG3',
+ 'SSADECISN',
+ 'INCOME',
+ 'TNUM_PS',
+ 'PSCHNG5',
+ 'PSPLANS5',
+ 'WHYCHNGD7',
+ 'CHNGHOW9',
+ 'CHNGHOW12',
+ 'PSWHYCHG4',
+ 'WORRY',
+ 'WHYCHNGD4',
+ 'NOTGET',
+ 'SSAPGM5',
+ 'PSCHNG4',
+ 'SPNDSRC6',
+ 'PRESCRIPT',
+ 'SSA_APPLY',
+ 'HLTHINS2',
+ 'PSCHNG1',
+ 'SPNDSRC5',
+ 'SPNDSRC2',
+ 'SPNDSRC8',
+ 'WHYCHNGD6',
+ 'ABIRTH_YEAR',
+ 'SSAEXPCT5',
+ 'PSPLANS4',
+ 'SSAPGM3',
+ 'WHYCHNGD9',
+ 'WHEREFREE5',
+ 'TEACH3',
+ 'RENTCUR',
+ 'WHEREFREE3',
+ 'PUBHLTH',
+ 'ENROLL1',
+ 'PSCHNG3',
+ 'FOODSUFRSN1',
+ 'SSALIKELY',
+ 'ENROLL3',
+ 'WHYCHNGD13',
+ 'UI_APPLY',
+ 'HLTHINS7',
+ 'DELAY',
+ 'RHISPANIC',
+ 'PWEIGHT',
+ 'AHISPANIC',
+ 'PSWHYCHG5',
+ 'TW_START',
+ 'CHNGHOW4',
+ 'INTRNT3',
+ 'TEACH1',
+ 'INTRNT2',
+ 'FEWRTRANS',
+ 'SSA_RECV',
+ 'HWEIGHT',
+ 'AGENDER',
+ 'INTRNTAVAIL',
+ 'SPNDSRC4',
+ 'MS',
+ 'CHNGHOW2',
+ 'TEACH2',
+ 'PSWHYCHG7',
+ 'PSWHYCHG1',
+ 'EST_ST',
+ 'TSPNDFOOD',
+ 'WHYCHNGD2',
+ 'LIVQTR',
+ 'CHNGHOW8',
+ 'SPNDSRC3',
+ 'COMPAVAIL',
+ 'FOODSUFRSN3',
+ 'SCRAM',
+ 'COMP2',
+ 'PSCHNG2',
+ 'CHNGHOW10',
+ 'PSPLANS6',
+ 'SSAPGM4',
+ 'ARACE',
+ 'WHEREFREE1',
+ 'HLTHINS1',
+ 'FOODSUFRSN2',
+ 'CHNGHOW6',
+ 'EEDUC',
+ 'SSAEXPCT3',
+ 'PLNDTRIPS',
+ 'PSWHYCHG9',
+ 'UI_RECV',
+ 'MH_NOTGET',
+ 'ENROLL2',
+ 'WHYCHNGD11',
+ 'HLTHINS3',
+ 'PSPLANS2',
+ 'WHYCHNGD10',
+ 'TBIRTH_YEAR',
+ 'PSWHYCHG8',
+ 'PSPLANS3',
+ 'THHLD_NUMPER',
+ 'WRKLOSS',
+ 'SNAP_YN',
+ 'CHNGHOW11',
+ 'PSCHNG7',
+ 'PSCHNG6',
+ 'MORTCONF',
+ 'CHNGHOW3',
+ 'COMP3',
+ 'ANXIOUS',
+ 'EVICT',
+ 'ANYWORK',
+ 'CURFOODSUF',
+ 'PSWHYCHG6',
+ 'SCHLHRS',
+ 'WHEREFREE6',
+ 'SSAEXPCT4',
+ 'FOODSUFRSN5',
+ 'WHYCHNGD3',
+ 'THHLD_NUMKID',
+ 'TEACH4',
+ 'SSAEXPCT1',
+ 'TEACH5',
+ 'CHNGHOW5',
+ 'PSWHYCHG2',
+ 'TSTDY_HRS',
+ 'WHYCHNGD12',
+ 'MORTCUR',
+ 'AHHLD_NUMKID',
+ 'PRIVHLTH',
+ 'AEDUC',
+ 'EXPNS_DIF',
+ 'TENURE',
+ 'WHYCHNGD8',
+ 'WHYCHNGD5',
+ 'HLTHINS8',
+ 'KINDWORK',
+ 'TCH_HRS',
+ 'WHEREFREE2',
+ 'DOWN',
+ 'INTRNT1',
+ 'EGENDER',
+ 'REGION',
+ 'RRACE',
+ 'THHLD_NUMADLT',
+ 'CHNGHOW1',
+ 'CHNGHOW7',
+ 'SPNDSRC1',
+ 'PSPLANS1',
+ 'EST_MSA',
+ 'RSNNOWRK',
+ 'FREEFOOD',
+ 'HLTHINS4',
+ 'WEEK',
+ 'COMP1',
+ 'CHILDFOOD',
+ 'FOODSUFRSN4']
+
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 CROSSWALK_SPREADSHEET_ID = '1xrfmQT7Ub1ayoNe05AQAFDhqL7qcKNSW6Y7XuA8s8uo'
 CROSSWALK_SHEET_NAMES = ['question_mapping', 'response_mapping', 'county_metro_state']
 
-DATA_COL_NAMES = ['variable', 'value']
+def check_housing_file_exists(housing_datafile: Path):
+    '''
+    check whether housing data csv already exists and set parameters accordingly
+    '''
+    if housing_datafile.exists():
+        week = housing_datafile.read_text().splitlines()[-1].split(',')[1]
+        mode, header = ('a', False)
+    else:
+        week = 13
+        mode, header = ('w', True)
+    return week, mode, header
 
 def data_url_str(w: int, wp: int):
-    return f"wk{w}/HPS_Week{wp}_PUF_CSV.zip"
+    year = '2021' if int(w) > 21 else '2020'
+    return f"{year}/wk{w}/HPS_Week{wp}_PUF_CSV.zip"
 
-def stack_df(df, col_names):
-    return pd.DataFrame(df.set_index(['SCRAM', 'WEEK']).stack()).reset_index().rename(
-        columns={'level_2':col_names[0], 0: col_names[1]})
+def data_file_str(wp: int, f: str):
+    year = '2021' if int(wp) > 21 else '2020'
+    if f == 'd':
+        return f"pulse{year}_puf_{wp}.csv"
+    elif f == 'w':
+        return f"pulse{year}_repwgt_puf_{wp}.csv"
 
-def get_puf_data(data_str: str, i: int, base_url: str = "https://www2.census.gov/programs-surveys/demo/datasets/hhp/2020/"):
+def download_housing_data(housing_datafile: Path, week: int, mode: str, header):
+    '''
+    
+    '''
+    r = True
+    while r:
+        week_pad = str(week).zfill(2)
+        data_str = data_url_str(week, week_pad)
+        week_df = get_puf_data(data_str, week_pad)
+        if week_df is None:
+            r = False
+        else:
+            week_df.to_csv(housing_datafile, mode=mode, header=header, index=False)
+            header, mode = (False, 'a')
+            week += 1
+
+def get_puf_data(data_str: str, wp: int,
+                 base_url: str = "https://www2.census.gov/programs-surveys/demo/datasets/hhp/"):
     '''
     download puf files for the given weeks and concatenate the datasets
     '''
     url = base_url + data_str
-    print(url)
+    print("trying: " + url)
     r = requests.get(url)
     if not r:
         print("url does not exist: {}".format(url))
         return None
     read_zip = zipfile.ZipFile(io.BytesIO(r.content))
-    data_df = pd.read_csv(read_zip.open("pulse2020_puf_{}.csv".format(i)), dtype={'SCRAM': 'string'})
-    weight_df = pd.read_csv(read_zip.open("pulse2020_repwgt_puf_{}.csv".format(i)), dtype={'SCRAM': 'string'})
+    data_df = pd.read_csv(read_zip.open(data_file_str(wp, 'd')), dtype={'SCRAM': 'string'}, usecols=PUF_USECOLS)
+    weight_df = pd.read_csv(read_zip.open(data_file_str(wp, 'w')), dtype={'SCRAM': 'string'})
     return data_df.merge(weight_df, how='left', on=['SCRAM', 'WEEK'])
 
 def get_crosswalk_sheets(service_account_file: Path):
     '''
-    downloads data sheets from houehold_pulse_data_dictionary crosswalks
+    download data sheets from houehold_pulse_data_dictionary crosswalks
     '''
     creds = service_account.Credentials.from_service_account_file(service_account_file, scopes=SCOPES)
     service = build('sheets', 'v4', credentials=creds)
@@ -68,21 +270,14 @@ def get_label_recode_dict(response_mapping: pd.DataFrame):
         d[row['variable']][row['value']] = row['label_recode']
     return d
 
-def get_feature_lists(question_mapping: pd.DataFrame, col_var: str):
-    '''
-    returns list of columns for col_var group (where col_var == 1)
-    '''
-    return list(question_mapping['variable'][question_mapping[col_var]=='1'])
-
 def generate_crosstab(df: pd.DataFrame, group_level: str, weight_var: str):
     '''
     generate crosstabs (from Nico's example code)
     '''
     crosstab = df.groupby([group_level]+['question_group', 'question_val', 'xtab_group', 'xtab_val']).agg({weight_var: 'sum'}).reset_index()
     crosstab["weight_total"] = crosstab.groupby([group_level]+['question_group','xtab_group'])[weight_var].transform('sum')
-    crosstab["share"] = crosstab[weight_var]/df3.weight_total
+    crosstab["share"] = crosstab[weight_var]/crosstab.weight_total
     crosstab["weight_total_val"] = crosstab.groupby([group_level]+['question_group','xtab_group','xtab_val'])[weight_var].transform('sum')
     crosstab["share_val"] = crosstab[weight_var]/crosstab.weight_total_val
-    crosstab.sort_values(by=[group_level]+['question_group', 'xtab_group'], inplace=True, ascending=True)
-    return crosstab
+    return crosstab.sort_values(by=[group_level]+['question_group', 'xtab_group'], ascending=True)
 
--- eviction_data_modelling.py
+++ eviction_data_modelling.py
@@ -7,56 +7,44 @@ data_dir = root/"data"
 
 data_dir.mkdir(exist_ok=True)
 
+###### download housing data
+# downloads full set of weeks (from week 13 onwards) or just new weeks if housing_datafile already exists
+housing_datafile = data_dir/"puf_housing_data_raw.csv"
+week, mode, header = check_housing_file_exists(housing_datafile)
+download_housing_data(housing_datafile, week, mode, header)
+print("finished downloading data")
+
+###### load full wide table
+df = pd.read_csv(housing_datafile)
+
+###### map in response labels into wide table
 service_account_file = root/"credentials.json"
 
-data = []
-r = True
-week = 13
-
-while r:
-    week_pad = str(week).zfill(2)
-    data_str = data_url_str(week, week_pad)
-    week_df = get_puf_data(data_str, week_pad)
-    if week_df is None:
-        r = False
-    else:
-        data.append(week_df)
-        week += 1
-
-df = pd.concat(data)
-
-# map in response labels into wide table
 question_mapping, response_mapping, county_metro_state = get_crosswalk_sheets(service_account_file)
 label_recode_dict = get_label_recode_dict(response_mapping)
 
 df_replaced = df.replace(label_recode_dict)
 df_replaced['topline'] = 1
+# save mapped labels version as csv
+df_replaced.to_csv(data_dir/"puf_housing_data_remapped_labels.csv")
 
-
-# column groups lists:
-stacked_crosstab_features = get_feature_lists(question_mapping, 'stacked_crosstab_features')
-multi_index_cols = get_feature_lists(question_mapping, 'multi_index_cols')
-stacked_question_features = get_feature_lists(question_mapping, 'stacked_question_features')
-
-
-#####################################
+###### generate crosstabs
 # replicating Nico's crosstab example
 weight_var = 'PWEIGHT'
 group_level = 'EST_MSA'
-multi_index_vars =  ['SCRAM'] + [group_level] + [weight_var]
+multi_index_vars =  ['SCRAM'] + [group_level] 
 stacked_crosstab_vars = ['topline', 'EEDUC','EGENDER']
 question_vars = ['RENTCUR', 'MORTCONF', 'EVICT']
 
-df1 = df_replaced.melt(id_vars=multi_index_vars+stacked_crosstab_vars, 
+df1 = df_replaced.melt(id_vars=multi_index_vars+stacked_crosstab_vars+[weight_var], 
                        value_vars=question_vars,
                        var_name='question_group',
                        value_name='question_val')
 
-df2 = df1.melt(id_vars=multi_index_vars+['question_group','question_val'],
+df2 = df1.melt(id_vars=multi_index_vars+['question_group','question_val']+[weight_var],
                value_vars=stacked_crosstab_vars,
                var_name='xtab_group',
                value_name='xtab_val')
 
 crosstab = generate_crosstab(df2, group_level, weight_var)
 
-# df_replaced.to_csv(data_dir/"puf_data.csv", index=False)
