28dc44fdb6d8b81108f9a6fac9a83f1e94987410
Caitlin Loftus <caitlinmloftus@gmail.com>
2021-02-24 17:19:45 -0600

    fixed week num bug and reworked mapping labels in slightly

--- eviction_data_modelling.py
+++ eviction_data_modelling.py
@@ -3,40 +3,52 @@ from pathlib import Path
 from etl import *
 
 root = Path.cwd()
-data_dir = root/"data"
 
+data_dir = root/"data"
 data_dir.mkdir(exist_ok=True)
 
-###### download housing data
-# downloads full set of weeks (from week 13 onwards) or just new weeks if housing_datafile already exists
-housing_datafile = data_dir/"puf_housing_data_raw.csv"
-week, mode, header = check_housing_file_exists(housing_datafile)
-download_housing_data(housing_datafile, week, mode, header)
-print("finished downloading data")
-
-###### load full wide table
-df = pd.read_csv(housing_datafile)
-
-###### map in response labels into wide table
 service_account_file = root/"credentials.json"
 
+###### download housing data
+raw_housing_datafile = data_dir/"puf_housing_data_raw.csv"
+remapped_housing_datafile = data_dir/"puf_housing_data_remapped_labels.csv"
+week, mode, header = check_housing_file_exists(raw_housing_datafile)
+
+# download crosswalk mapping tables
 question_mapping, response_mapping, county_metro_state = get_crosswalk_sheets(service_account_file)
 label_recode_dict = get_label_recode_dict(response_mapping)
 
-df_replaced = df.replace(label_recode_dict)
-df_replaced['topline'] = 1
-# save mapped labels version as csv
-df_replaced.to_csv(data_dir/"puf_housing_data_remapped_labels.csv")
+# downloads full set of weeks (from week 13 onwards) or just new weeks if housing_datafile already exists
+r = True
+while r:
+    week_pad = str(week).zfill(2)
+    data_str = data_url_str(week, week_pad)
+    week_df = get_puf_data(data_str, week_pad)
+    if week_df is None:
+        r = False
+    else:
+        week_df.to_csv(raw_housing_datafile, mode=mode, header=header, index=False)
+        week_df.replace(label_recode_dict).to_csv(remapped_housing_datafile, mode=mode, header=header, index=False)
+        header, mode = (False, 'a')
+        week += 1
+
+print("finished downloading data")
+
 
 ###### generate crosstabs
 # replicating Nico's crosstab example
+
+# load full wide table
+df = pd.read_csv(remapped_housing_datafile)
+df['topline'] = 1
+
 weight_var = 'PWEIGHT'
 group_level = 'EST_MSA'
 multi_index_vars =  ['SCRAM'] + [group_level] 
 stacked_crosstab_vars = ['topline', 'EEDUC','EGENDER']
 question_vars = ['RENTCUR', 'MORTCONF', 'EVICT']
 
-df1 = df_replaced.melt(id_vars=multi_index_vars+stacked_crosstab_vars+[weight_var], 
+df1 = df.melt(id_vars=multi_index_vars+stacked_crosstab_vars+[weight_var], 
                        value_vars=question_vars,
                        var_name='question_group',
                        value_name='question_val')
@@ -48,3 +60,5 @@ df2 = df1.melt(id_vars=multi_index_vars+['question_group','question_val']+[weigh
 
 crosstab = generate_crosstab(df2, group_level, weight_var)
 
+
+# remove missing: data_df[-data_df.isin([-88,-99]).any(axis=1)]
