c8491aadb8e34081c2a9d131b30c3ed934d24b07
Caitlin Loftus <caitlinmloftus@gmail.com>
2021-05-20 17:13:28 -0500

    small change to upload to gcp bucket code, and moved functions from etl.py into eviction_data_modelling.py for databricks

--- prod/eviction_data_modelling.py
+++ prod/eviction_data_modelling.py
@@ -1,6 +1,204 @@
 from pathlib import Path
-from etl import *
+from typing import Dict, Optional, Sequence, Tuple
+
 import zipfile
+import io
+import re
+import os
+import requests
+import numpy as np
+import pandas as pd
+import numpy as np
+from bs4 import BeautifulSoup
+
+from googleapiclient.discovery import build
+from google.oauth2 import service_account
+from google.cloud import storage
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+from oauth2client.service_account import ServiceAccountCredentials
+
+SHEETS_SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+CROSSWALK_SPREADSHEET_ID = '1xrfmQT7Ub1ayoNe05AQAFDhqL7qcKNSW6Y7XuA8s8uo'
+CROSSWALK_SHEET_NAMES = ['question_mapping', 'response_mapping', 'county_metro_state']
+DRIVE_SCOPES = ['https://www.googleapis.com/auth/drive']
+GDRIVE_ID = '14LK-dEay1G9UpBjXw6Kt9eTXwZjx8rj9'
+
+NUMERIC_COL_BUCKETS = {
+    'TBIRTH_YEAR': {'bins': [1920, 1957, 1972, 1992, 2003],
+                    'labels': ['65+','50-64','30-49','18-29']},
+    'THHLD_NUMPER': {'bins': [0, 3, 6, 10, 99],
+                    'labels': ['1-2','3-5','6-9','10+']},
+    'THHLD_NUMKID': {'bins': [0, 1, 3, 6, 10, 40],
+                    'labels': ['0','1-2','3-5','6-9','10+']},
+    'THHLD_NUMADLT': {'bins': [0, 2, 6, 9, 40],
+                     'labels': ['1-2','3-5','6-9','10+']},
+    'TSPNDFOOD': {'bins': [0, 100, 300, 500, 800, 1000],
+                 'labels': ['0-99','100-299','300-499','500-799','800+']},
+    'TSPNDPRPD': {'bins': [0, 100, 200, 300, 400, 1000],
+                 'labels': ['0-99','100-199','200-299','300-399','400+']},
+    'TSTDY_HRS': {'bins': [0, 5, 10, 15, 20,50],
+                 'labels': ['0-4','5-9','10-14','15-19','20+']},
+    'TNUM_PS': {'bins': [0, 1, 2, 3, 4, 99],
+                'labels': ['0','1','2','3','4+']},
+    'TBEDROOMS': {'bins': [0, 1, 2, 3, 4, 99],
+                 'labels': ['0','1','2','3','4+']},
+    'TUI_NUMPER': {'bins': [0, 1, 2, 3, 4, 99],
+                  'labels': ['0','1','2','3','4+']},
+}
+
+def bucketize_numeric_cols(df: pd.DataFrame, question_mapping: pd.DataFrame):
+    num_cols = list(question_mapping['variable'][question_mapping['type_of_variable'] == 'NUMERIC'])
+    for col in num_cols:
+        if col in df.columns:
+            df[col] = pd.cut(df[col],
+                        bins=NUMERIC_COL_BUCKETS[col]['bins'], 
+                        labels=NUMERIC_COL_BUCKETS[col]['labels'], right=False)
+    return df
+
+def check_housing_file_exists(housing_datafile: Path):
+    '''
+    check whether housing data csv already exists and set parameters accordingly
+    '''
+    if housing_datafile.exists():
+        file = housing_datafile.read_text().splitlines()
+        week = int(file[-1].split(',')[1]) + 1
+        mode, header = ('a', False)
+        cols = list(file[0].split(','))
+    else:
+        week = 13
+        mode, header, cols  = ('w', True, None)
+    return week, mode, header, cols
+
+def data_url_str(w: int, wp: int):
+    year = '2021' if int(w) > 21 else '2020'
+    return f"{year}/wk{w}/HPS_Week{wp}_PUF_CSV.zip"
+
+def data_file_str(wp: int, f: str):
+    year = '2021' if int(wp) > 21 else '2020'
+    if f == 'd':
+        return f"pulse{year}_puf_{wp}.csv"
+    elif f == 'w':
+        return f"pulse{year}_repwgt_puf_{wp}.csv"
+
+def get_puf_data(data_str: str, wp: int, 
+                 base_url: str = "https://www2.census.gov/programs-surveys/demo/datasets/hhp/"):
+    '''
+    download puf zip file for the given week and merge weights and puf dataframes
+    '''
+    url = base_url + data_str
+    r = requests.get(url)
+    print("Trying: {}".format(url))
+    if not r:
+        print("URL does not exist: {}".format(url))
+        return None
+    read_zip = zipfile.ZipFile(io.BytesIO(r.content))
+    data_df = pd.read_csv(read_zip.open(data_file_str(wp, 'd')), dtype={'SCRAM': 'string'})
+    weight_df = pd.read_csv(read_zip.open(data_file_str(wp, 'w')), dtype={'SCRAM': 'string'})
+    return data_df.merge(weight_df, how='left', on=['SCRAM', 'WEEK'])
+
+def get_crosswalk_sheets():
+    '''
+    N.B. this is the prod version for databricks (auth will not work on local machine - use dev version)
+    Download sheets from housing_pulse_data_dictionary into dataframes
+    returns:
+        list of pandas dataframes (3), the first three sheets in the google sheets  
+    '''
+    CROSSWALK_SHEET_NAMES = ['question_mapping', 'response_mapping', 'county_metro_state']
+    credentials, project_id = google.auth.default(scopes=['https://www.googleapis.com/auth/spreadsheets'])
+    service = build('sheets', 'v4', credentials=credentials)
+    sheet = service.spreadsheets()
+    spreadsheet_id = '1xrfmQT7Ub1ayoNe05AQAFDhqL7qcKNSW6Y7XuA8s8uo'
+    # Call the Sheets API
+    result_input = sheet.values().batchGet(spreadsheetId=spreadsheet_id,
+                                ranges=CROSSWALK_SHEET_NAMES).execute()
+    ranges = result_input.get('valueRanges', [])
+    data = []
+    for r in ranges[:3]:
+        values_input = r.get('values', [])
+        df = pd.DataFrame(values_input[1:], columns=values_input[0])
+        data.append(df)
+    return data
+
+def get_label_recode_dict(response_mapping: pd.DataFrame):
+    '''
+    Convert question response mapping df into dict to recode labels 
+    inputs:
+        response_mapping: pd.DataFrame, the response_mapping df from the household_publse_data_dictionary google sheet
+    returns: dictionary - {variable: {value: label_recode}}
+    '''
+    response_mapping_df = response_mapping[response_mapping['do_not_join']=='0']
+    response_mapping_df['value'] = response_mapping_df['value'].astype('float64')
+    d = {}
+    for i, row in response_mapping_df[['variable','value','label_recode']].iterrows():
+        if row['variable'] not in d.keys():
+            d[row['variable']] = {}
+        d[row['variable']][row['value']] = row['label_recode']
+    return d
+
+def get_std_err(df, weight):
+    #make 1d array of weight col
+    obs_wgts = df[weight].to_numpy().reshape(len(df),1)
+    #make 80d array of replicate weights
+    rep_wgts = df[[i for i in df.columns if weight in i and not i == weight]].to_numpy()
+    #return standard error of estimate
+    return np.sqrt((np.sum(np.square(rep_wgts-obs_wgts),axis=1)*(4/80)))
+
+def filter_non_weight_cols(cols_list):
+    '''
+    Helper function to filter columns in dataframe to just variable columns (removes weight columns)
+    inputs:
+        cols_list: list of strings, the column names of the dataframe
+    returns: list of strings, the column names with the WEIGHT column names removed
+    '''
+    r = re.compile("(?!.*WEIGHT\d+)")
+    return list(filter(r.match, cols_list))
+
+def upload_to_cloud_storage(bucket_name: str, df: pd.DataFrame, filename: str):
+    '''
+    Uploads a dataframe to cloud storage bucket.
+    inputs:
+        bucket_name: string, name of the bucket to upload to 
+        df: pd.DataFrame to be uploaded to cloud storage
+        filename: string, name of file in cloud storage
+    '''
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+    blob = bucket.blob(filename)
+    blob.upload_from_string(df.to_csv(), 'text/csv', timeout=450)
+    print('File uploaded to {}:{}.'.format(bucket_name, filename))
+
+def upload_to_gdrive(upload_filename: str):
+    '''
+    Uploads crosstabs csv to gdrive folder.
+    inputs:
+        service_account_file: string, path to service account file 
+        upload_filename: string, name of file on gdrive
+    '''
+    gauth = GoogleAuth()
+    gauth.credentials, project_id = google.auth.default(scopes=['https://www.googleapis.com/auth/spreadsheets'])
+    drive = GoogleDrive(gauth)
+    gfile = drive.CreateFile({'parents': [{'id': GDRIVE_ID}], 'title': 'crosstabs.csv'})
+    gfile.SetContentFile(upload_filename)
+    gfile.Upload()
+    print("uploaded file to gdrive")
+
+def week_mapper():
+    URL = 'https://www.census.gov/programs-surveys/household-pulse-survey/data.html'
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    week_dict = {}
+    for i in soup.find_all("p", class_="uscb-margin-TB-02 uscb-title-3"):
+        label = i.text.strip('\n\t\t\t')
+        if 'Week' in label:
+            kv_pair = label.split(':')
+            week_int = int(re.sub("[^0-9]", "", kv_pair[0]))
+            if week_int > 21:
+                dates = kv_pair[1][1:] + ' 2021'
+            else:
+                dates = kv_pair[1][1:] + ' 2020'
+            week_dict[week_int] = dates
+    return week_dict
 
 def freq_crosstab(df, col_list, weight, critical_val=1):
     pt_estimates = df.groupby(col_list, as_index=True)[[i for i in df.columns if weight in i]].agg('sum')
@@ -149,16 +347,6 @@ if __name__=="__main__":
     #natl_level = natl_level.merge(question_mapping[['description_recode', 'variable']],
     #                              left_on='question', right_on='variable')
 
-    #crosstabs.to_csv(data_dir/'crosstabs.csv', index=False)
-    #crosstabs_nat.to_csv(data_dir/'crosstabs_national.csv', index=False)
+    upload_to_cloud_storage("household-pulse-bucket", crosstabs, "crosstabs.csv")
+    upload_to_cloud_storage("household-pulse-bucket", crosstabs_nat, "crosstabs_national.csv")
 
-    crosstabs.to_csv(data_dir/'crosstabs.csv', index=False)
-    upload_to_cloud_storage("crosstabs_output", crosstabs, "crosstabs.csv")
-    upload_to_cloud_storage("crosstabs_output", crosstabs_nat, "crosstabs_national.csv")
-
-    zf = zipfile.ZipFile("crosstabs.csv.zip", mode="w")
-    compression = zipfile.ZIP_DEFLATED
-    # upload_to_gdrive(data_dir/'crosstabs.csv')
-    #upload_to_gdrive(SERVICE_ACCOUNT_FILE, data_dir/'crosstabs_national.csv')
-
-    # export_to_sheets(crosstabs,'flat_file',service_account_file)
