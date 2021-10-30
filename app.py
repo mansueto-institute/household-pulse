50ed2ee01d81fe2c2861827129f73fb8df3096cd
Manuel Martinez <manmartgarc@gmail.com>
2021-10-29 19:17:49 -0500

    add lambda handler module

---
+++ app.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Friday, 29th October 2021 3:14:07 pm
+===============================================================================
+@filename:  app.py
+@author:    Manuel Martinez (manmart@uchicago.edu)
+@project:   household-pulse
+@purpose:   this is the lambda function that goes into the dockerfile
+===============================================================================
+"""
+import subprocess
+
+
+def handler(event, context):
+    args = event['args'].split()
+    proc = subprocess.run(
+        ['python3', '-m', 'household_pulse', *args],
+        capture_output=True)
+    return proc.stdout
