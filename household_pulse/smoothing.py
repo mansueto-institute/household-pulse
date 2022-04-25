30139da1b6cadbe349f897e3af0e03b8b4741b46
Manuel Martinez <manmartgarc@gmail.com>
2022-04-24 19:44:20 -0500

    add basic script to perform smoothing

--- household_pulse/smoothing.py
+++ household_pulse/smoothing.py
@@ -10,43 +10,57 @@ Created on Friday, 22nd April 2022 5:09:03 pm
             values across time.
 ===============================================================================
 """
-import matplotlib.pyplot as plt
+
 import pandas as pd
 import statsmodels.api as sm
-from tqdm import tqdm
 
 from household_pulse.mysql_wrapper import PulseSQL
+
+
+def smooth_group(group: pd.DataFrame, frac: float = 0.2) -> pd.DataFrame:
+    wcols = [
+        'pweight_share',
+        'pweight_lower_share',
+        'pweight_upper_share',
+        'hweight_share',
+        'hweight_lower_share',
+        'hweight_upper_share'
+    ]
+    for wcol in wcols:
+        smoothed = sm.nonparametric.lowess(
+            exog=group['week'],
+            endog=group[wcol],
+            frac=frac)
+        group[f'{wcol}_smoothed'] = smoothed[:, 1]
+        group.drop(columns=wcol, inplace=True)
+
+    return group
+
 
 if __name__ == "__main__":
     sql = PulseSQL()
 
     query = '''
-        SELECT *
+        SELECT week,
+            xtab_var,
+            xtab_val,
+            q_var,
+            q_val,
+            pweight_share,
+            pweight_lower_share,
+            pweight_upper_share,
+            hweight_share,
+            hweight_lower_share,
+            hweight_upper_share
         FROM pulse
     '''
+
     df = sql.get_pulse_table(query=query)
-    print('done download')
 
-    frac = 0.20
-    grouped = (
-        df
-        .groupby(['xtab_var', 'xtab_val', 'q_var', 'q_val'])
-        .apply(lambda x: sm.nonparametric.lowess(
-            exog=x['week'],
-            endog=x['pweight_share'],
-            frac=0.2))
-    )
-    cols = ['xtab_var', 'xtab_val', 'q_var', 'q_val']
-    auxdfs = []
-    for row in tqdm(grouped.iteritems()):
-        index, values = row
-        auxdf = pd.DataFrame(
-            values,
-            columns=('week', 'pweight_share_smoothed'))
-
-        for col, val in zip(cols, index):
-            auxdf[col] = val
-        auxdfs.append(auxdf)
-
-    test = pd.concat(auxdfs)
-    df = df.merge(test, on=['week', 'xtab_var', 'xtab_val', 'q_var', 'q_val'])
+    df = df.groupby(['xtab_var', 'xtab_val', 'q_var', 'q_val']).apply(
+        smooth_group)
+    df.sort_values(
+        by=['week', 'xtab_var', 'xtab_val', 'q_var', 'q_val'],
+        inplace=True)
+    sql.update_values(table='smoothed', df=df)
+    sql.close_connection()
