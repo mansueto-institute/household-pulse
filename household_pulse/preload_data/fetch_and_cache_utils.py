0d586f7e458a47738608040deb30c718090087e0
Manuel Martinez <manmartgarc@gmail.com>
2022-05-23 13:08:02 -0500

    add missing import

--- household_pulse/preload_data/fetch_and_cache_utils.py
+++ household_pulse/preload_data/fetch_and_cache_utils.py
@@ -7,6 +7,7 @@ from os import environ, path
 import boto3
 import pandas as pd
 import sqlalchemy as db
+from botocore.exceptions import ClientError
 
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
