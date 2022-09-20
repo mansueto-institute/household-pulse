ea1470178dadcc5ee9c495152317523dccf872d2
Manuel Martinez <manmartgarc@gmail.com>
2022-09-19 20:21:38 -0400

    test(pkg): add basic import test

---
+++ tests/test_household_pulse.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Monday, 19th September 2022 7:29:31 pm
+===============================================================================
+@filename:  test_household_pulse.py
+@author:    Manuel Martinez (manmart@uchicago.edu)
+@project:   household-pulse
+@purpose:   Package-level import unit tests.
+===============================================================================
+"""
+# pylint: disable=unused-import,missing-function-docstring
+# pylint: disable=import-outside-toplevel
+
+
+def test_import_top_level():
+    import household_pulse  # noqa: F401
