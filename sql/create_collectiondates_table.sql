3dddf171baca4654863607af337b3125d4ceeda8
Manuel Martinez <manmartgarc@gmail.com>
2023-01-21 00:28:32 -0600

    fix: remove sql scripts

--- sql/create_collectiondates_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `pulse`.`collection_dates` (
-    `week` INT NOT NULL,
-    `pub_date` DATE NOT NULL,
-    `start_date` DATE NOT NULL,
-    `end_date` DATE NOT NULL,
-    PRIMARY KEY (`week`, `pub_date`, `start_date`, `end_date`)
-);
